351860573c886388c7de685263592f50c6f42026
Dzianis (Denis) Tsialitsyn <telicinda@gmail.com>
2024-04-24 06:43:07 +0300

    fixes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => (
     <div>
-        <h1>Hi there! With Workflow!!!</h1>
+        <h1>Hi there! With Workflow!</h1>
         <hr/>
         <MarketingApp/>
     </div>
